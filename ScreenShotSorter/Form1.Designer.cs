90f44ab52ebca8644ec56c09db1cbd6b744ed734
ao1415 <taku31126816@yahoo.co.jp>
2018-03-31 20:24:28 +0900

    アイコン変更

--- ScreenShotSorter/Form1.Designer.cs
+++ ScreenShotSorter/Form1.Designer.cs
@@ -83,6 +83,7 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(120, 0);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
 			this.Opacity = 0D;
 			this.ShowInTaskbar = false;
